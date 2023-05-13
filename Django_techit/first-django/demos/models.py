d9b7ee546b4dfd488f7a7a9c2d71dc1dfc986ed6
chaehyeon02 <rhee2015@gmail.com>
2023-05-14 04:58:41 +0900

    [Create] header.html and css
    헤더 틀 잡아두기
    검색창 및 로그인 부분 수정 필요
    로고는 임시로 캡쳐해서 넣었음

--- Django_techit/first-django/demos/models.py
+++ Django_techit/first-django/demos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Comment(models.Model) {
+    content = models.TextField(verbose_name='내용')
+    created_at = models.DataTimeField(verbose_name='작성일')
+    post = models.ForeignKey(to='Post', on_delete=models.CASCADE)
+    writer = models.ForeignKey(to=User, on_delete=models.CASCADE, null=true, blank=true)
+}
